5fbdedec5fdd2e29600e7b1bb8685e2ddf6c7f7d
Christian Menz <christianmenz@gmail.com>
2015-09-13 14:22:53 +0200

    initial commit

---
+++ app.ts
@@ -0,0 +1,12 @@
+/// <reference path="typings/node/node.d.ts" />
+
+
+class App {
+    public static main(): number {
+        console.log('Hello World');
+        return 0;
+    }
+}
+
+
+App.main();
---
+++ bin/app.js
@@ -0,0 +1,12 @@
+/// <reference path="typings/node/node.d.ts" />
+var App = (function () {
+    function App() {
+    }
+    App.main = function () {
+        console.log('Hello World');
+        return 0;
+    };
+    return App;
+})();
+App.main();
+//# sourceMappingURL=app.js.map
